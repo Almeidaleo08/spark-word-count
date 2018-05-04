21181e21164e2d45ace4f8408b418cc6ba26c8a0
Leonardo de Almeida <leonardo.almeida@rivendel.com.br>
2018-05-04 14:44:21 -0300

    prepared to receive s3 files

--- src/main/scala/br/com/rivendel/spark/Main.scala
+++ src/main/scala/br/com/rivendel/spark/Main.scala
@@ -7,18 +7,13 @@ object Main {
 
     def main(args: Array[String]): Unit = {
 
-        var file = ""
-
-        if (args.length > 0) {
-            file = args(0)
-        } else {
-            file = "src/main.resources/text.txt"
-        }
+        val file = args(0)
+        val outputDir = args(1)
 
         Logger.getLogger("org").setLevel(Level.ERROR)
         val conf = new SparkConf()
             .setAppName("WordCount")
-            .setMaster("local[*]")
+
         val sc = new SparkContext(conf)
 
         val lines = sc.textFile(file)
@@ -27,9 +22,7 @@ object Main {
         val wordCounts = words.countByValue()
 
         val rdd = sc.parallelize(wordCounts.toSeq)
-        rdd.coalesce(1).saveAsTextFile("./wordCount")
-
-        for ((word, count) <- wordCounts) println(word + " : " + count)
+        rdd.saveAsTextFile(outputDir)
     }
 
 }
